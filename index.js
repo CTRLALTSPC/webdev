2f2e9d43324ea9ddb57b6199f6f2d8f8ce2a83b2
CTRLALTPSC <estroyny0@gmail.com>
2021-11-05 15:32:55 -0500

    11.5

---
+++ index.js
@@ -0,0 +1,19 @@
+// index.js 
+
+// var number = 5;
+// let name = "Blaise";
+
+// number = 12;  // executed from the top down so the number would print 12 
+
+// // writing to the document
+// document.writeln(name + number);
+// alert("Hello World");  -- this will be ignored --
+
+let hasJob = true; 
+    if (hasJob) {
+         // have a job
+      document.writeln("I am employed, thanks");
+    } else {
+        // I need a job  
+      document.writeln("I am looking for a position, thanks");
+    }
