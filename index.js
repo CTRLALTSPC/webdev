aa75c53ea907e7cb0000a73064565cdb8162e499
CTRLALTPSC <estroyny0@gmail.com>
2021-11-11 09:56:19 -0600

    11.11

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 // document.writeln(name + number);
 // alert("Hello World");  -- this will be ignored --
 
-let hasJob = true; 
+let hasJob = false; 
     if (hasJob) {
          // have a job
       document.writeln("I am employed, thanks");
@@ -17,3 +17,5 @@ let hasJob = true;
         // I need a job  
       document.writeln("I am looking for a position, thanks");
     }
+
+  let today = new Date(); // assign today to an object which is new date  
