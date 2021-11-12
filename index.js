f0722092dad395c9b888a219a7aea90cfa21bf31
CTRLALTPSC <estroyny0@gmail.com>
2021-11-12 13:46:23 -0600

    11.12 js basics

--- index.js
+++ index.js
@@ -9,13 +9,24 @@
 // document.writeln(name + number);
 // alert("Hello World");  -- this will be ignored --
 
-let hasJob = false; 
+let hasJob = true; 
     if (hasJob) {
          // have a job
-      document.writeln("I am employed, thanks");
+      showMessage("I am employed, thanks");
     } else {
         // I need a job  
-      document.writeln("I am looking for a position, thanks");
+      showMessage("I am looking for a position, thanks");
     }
 
-  let today = new Date(); // assign today to an object which is new date  
+  let today = new Date("11/01/2021"); // assign today to an object which is new date  
+  let dayOfWeek = today.getDay();  // 0 represents sunday
+  if (dayOfWeek === 0 || dayOfWeek === 6) { // and = && or || 
+    showMessage("Since it is the weekend, please be patient.");
+  }
+
+  //functions ; small piece of code over and over again
+
+  function showMessage(message) { // pass in message to function
+    document.writeln("<p>" + message + "</p>");  // to change things later, then theres only one spot to change
+    document.writeln("<hr/>");  // hr creates a horizontal line on the page
+  }
